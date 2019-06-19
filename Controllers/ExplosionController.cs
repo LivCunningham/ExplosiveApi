c02aadfb827f10f8df65bd0e67c1358a7dd8e970
Olivia Cunningham <Liv.cunninghamgb@gmail.com>
2019-06-19 08:12:39 -0400

    explosive completed and mumble working

---
+++ Controllers/ExplosionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExplosiveApi.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class ExplosionController : ControllerBase
+  {
+    [HttpGet("{input}")]
+    public ActionResult<string> GetExplosion(string input)
+    {
+      var res = "";
+      foreach (var c in input)
+      {
+        res += new String(c, int.Parse(c.ToString()));
+      }
+      return res;
+    }
+  }
+}
